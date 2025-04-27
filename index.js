e8c49e61ec06ce46eac8b7b4b118ec09132d54f1
rifkiilhamlutfika-dev <rifkiilhamlutfika.dev@gmail.com>
2025-04-27 15:40:09 +0700

    add page not found

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ const port = 8080 || 3000;
 
 const app = express();
 
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-  //   req.timeRequest = Date.now();
-  console.log(req.method, req.url);
+  req.timeRequest = Date.now();
+  //   console.log(req.method, req.url);
   next();
 });
 
 app.get("/", (req, res) => {
   res.send("hello world");
   console.log(req.timeRequest);
+});
+
+app.use((req, res) => {
+  res.status(404).send("Not Found");
 });
 
 app.listen(port, () => {
